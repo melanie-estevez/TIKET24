830e5bfa667b4508c301ede1b499a46e587dd0e7
JossGuerrero <xdearly12@gmail.com>
2024-12-02 11:28:57 -0500

    calculos.py

--- calculos.py
+++ calculos.py
@@ -2,3 +2,16 @@ def calcularCuota(monto, tasa_interes, meses):
     tasa_mensual = tasa_interes / 100 / 12
     cuota = monto * (tasa_mensual * (1 + tasa_mensual) ** meses) / ((1 + tasa_mensual) ** meses - 1)
     return cuota
+
+from datetime import datetime
+
+def calcularEdad(anioNacimiento):
+    anioActual = datetime.now().year
+    
+    if anioNacimiento < 0 or anioNacimiento > anioActual:
+        return -1
+    
+    edad = anioActual - anioNacimiento
+    return edad
+
+
