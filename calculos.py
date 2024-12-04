5ce5d112df66c077a605c6df3310cf3a981ef61a
melanie-estevez <melaniestevez@hotmail.com>
2024-12-04 08:58:48 -0500

    Update calculos.py

--- calculos.py
+++ calculos.py
@@ -14,6 +14,27 @@ def calcularEdad(anioNacimiento):
     edad = anioActual - anioNacimiento
     return edad
 
+def determinarResultadosIMC(imc):
+    if 0 <= imc <=16:
+        return "Delgadez severa"
+    elif 16 <imc <=17:
+        return "Delgadez moderada"
+    elif 17 < imc <=18.5:
+        return "Delgadez leve"
+    elif 18.5 < imc <=25:
+        return "Peso normal"
+    elif 25 < imc <= 30:
+        return "Sobrepeso"
+    elif 30 < imc <=35:
+        return "Obesidad grado 1"
+    elif 35 < imc <= 40:
+        return "Obesidad grado 2"
+    elif imc >40:
+        return "Obesidad grado 3"
+    else:
+        return "IMC fuera de rango"
+
+
 def encontrarMayor(a, b, c):
     return max(a, b, c)
 
